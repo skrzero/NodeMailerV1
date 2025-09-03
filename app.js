52a868f7bfdd2a4a8e73348d939ddb01b405ffde
skrzero <Formationafec@outlook.fr>
2025-09-03 13:59:08 +0200

    post + get

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-//import
-//import
-//import
+import express from "express";
+import bodyParser from "body-parser";
+import { body, validationResult } from "express-validator";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/welcome", (req, res) => {
+  res.send(`
+    <form method="POST" action="/welcome">
+      <input type="text" name="name" placeholder="Nom" />
+      <input type="email" name="email" placeholder="Email" />
+      <button type="submit">Envoyer</button>
+    </form>
+  `);
+});
+
+app.post(
+  "/welcome",
+  [
+    body("name").isLength({ min: 2}).withMessage("nom trop court"),
+    body("email").isEmail().withMessage("Email invalide")
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send("Erreur: " + JSON.stringify(errors.array()));
+    }
+
+    const { name, email } = req.body;
+  }
+);
+
+app.listen(3000, () => console.log("http://localhost:3000/welcome"));
