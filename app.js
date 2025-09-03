e4ffbcf4bd8c957e35d6b1fe9d60f730743e7de0
skrzero <Formationafec@outlook.fr>
2025-09-03 14:22:39 +0200

    modif

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ import welcomeRoute from "./routes/welcome.route.js";
 
 const app = express();
 
-// Middleware pour lire req.body
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Utilisation de la route /welcome
 app.use("/welcome", welcomeRoute);
 
 app.listen(3000, () =>
-  console.log("🚀 Serveur démarré sur http://localhost:3000/welcome")
+  console.log("Serveur démarré sur http://localhost:3000/welcome")
 );
